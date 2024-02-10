00f2d594a6e1d9bccc0d37de16e7e79d4e837242
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2024-02-10 20:21:40 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget -O server.py https://raw.githubusercontent.com/raiyan088/public/main/python/server.py
 wget -O worker.py https://raw.githubusercontent.com/raiyan088/public/main/python/worker.py
 wget -O module.js https://raw.githubusercontent.com/raiyan088/public/main/python/module.js
+pip install websocket-client
 pip install javascript
-pip install websocket
 python server.py
